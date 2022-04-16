c855c12ef9875d86607f6b25c27b9e029a1d3eb2
maxime1907 <maxime1907.dev@gmail.com>
2022-04-16 18:14:09 +0200

    Initial commit

---
+++ isgoodsig.py
@@ -0,0 +1,35 @@
+import idc
+import idaapi
+
+FUNCS_SEGEND = ida_segment.get_segm_by_name(".text").end_ea
+
+def main():
+	sig = ida_kernwin.ask_str("", 0, "Insert signature: ")
+
+	# wtfwtfwtfwtf
+	oldsig = sig
+	sig = sig.replace(r"\x", " ").replace("2A", "?").replace("2a", "?").strip()
+#	print(sig)
+
+	count = checksig(sig)
+	if not count:
+		print(r"INVALID: {}".format(oldsig))
+		print("Could not find any matching signatures for input")
+	elif count == 1:
+		print(r"VALID: {}".format(oldsig))
+	else:
+		print(r"INVALID: {}".format(oldsig))
+		print("Found {} instances of input signature".format(count))
+
+def checksig(sig):
+	count = 0
+	addr = 0
+	addr = ida_search.find_binary(addr, FUNCS_SEGEND, sig, 0, idc.SEARCH_DOWN|idc.SEARCH_NEXT)
+	while addr != idc.BADADDR:
+		count = count + 1
+		addr = ida_search.find_binary(addr, FUNCS_SEGEND, sig, 0, idc.SEARCH_DOWN|idc.SEARCH_NEXT)
+
+	return count
+
+if __name__ == "__main__":
+	main()
